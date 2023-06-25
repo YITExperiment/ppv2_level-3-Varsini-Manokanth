fbe94e7bc5d9a5eef44faf405604c2a0d314407d
Varsini-Manokanth <135005710+Varsini-Manokanth@users.noreply.github.com>
2023-06-25 13:43:11 +0530

    Update screen_pet.py

--- screen_pet.py
+++ screen_pet.py
@@ -13,7 +13,6 @@ def blink():
     toggle_eyes()
     root.after(250,toggle_eyes)
     root.after(3000,blink)
-
 root=Tk()
  
 c=Canvas(root,width=400,height=400)
@@ -24,6 +23,7 @@ body = c.create_oval(35,20,365,350,outline=c.body_color,fill=c.body_color)
 ear_left=c.create_polygon(75,80,75,10,165,70,outline=c.body_color,fill=c.body_color)
 ear_right=c.create_polygon(255,45,325,10,320,70,outline=c.body_color,
 fill=c.body_color)
+
 foot_left=c.create_oval(65,320,145,360,outline=c.body_color,fill=c.body_color)
 foot_right=c.create_oval(250,320,330,360,outline=c.body_color,fill=c.body_color)
 eye_left=c.create_oval(130,110,160,170,outline='black',fill='white')
